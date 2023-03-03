384e8d7c391f75c39dd7056fa5553edeb6bf4440
ayushibindroo <ayushibindroo10@gmail.com>
2023-03-03 14:33:13 +0530

    Update ConfigClass.py

--- features/utility/ConfigClass.py
+++ features/utility/ConfigClass.py
@@ -1,6 +1,6 @@
 class ConfigClass:
-    filePath = "C:\\Users\\Ayushi Bindroo\\PycharmProjects\\Sprint2Automation\\drivers\\chromedriver.exe"
+    filePath = "drivers\\chromedriver.exe"
 
     url = "https://www.payback.in/"
 
-    dataFilePath = "C:\\Users\\Ayushi Bindroo\\PycharmProjects\\Sprint2Automation\\datafiles\\LoginExcelsheetD.xlsx"
+    dataFilePath = "datafiles\\LoginExcelsheetD.xlsx"
